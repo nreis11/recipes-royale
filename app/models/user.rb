0ef15aa230edb697067902b175abaa5d7c1d0ca8
Jaime Rios & Marvan Taghvaie & Nick Reis & Ali Baker <pair+jaimejrios.marvant.reeses-pieces.thealicat13@devbootcamp.com>
2016-10-26 15:32:40 -0700

    Add validations and associations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :recipes, foreign_key: :creator_id
+  has_many :ratings, foreign_key: :rater_id
+
+  validates :first_name, :last_name, presence: true
 end
