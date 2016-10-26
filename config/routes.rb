5cef07b9798217c325011b6ebd164c6f941af4a0
Jaime Rios & Marvan Taghvaie & Nick Reis & Ali Baker <pair+jaimejrios.marvant.reeses-pieces.thealicat13@devbootcamp.com>
2016-10-26 14:58:38 -0700

    Migrate user, add home controllers, add user controllers via devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
-Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
-end
+    Rails.application.routes.draw do
+      devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+
+      root 'home#index'
+    end
