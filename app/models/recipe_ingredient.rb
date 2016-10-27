45589916266bca4a466923473e1268087dfdbf27
Jaime Rios & Marvan Taghvaie & Nick Reis & Ali Baker <pair+jaimejrios.marvant.reeses-pieces.thealicat13@devbootcamp.com>
2016-10-26 17:29:37 -0700

    Add awesome print gem, fix association in recipe_ingredients table, test associations. Ready to rock

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,6 +1,6 @@
 class RecipeIngredient < ApplicationRecord
-  belongs_to :ingredients
-  belongs_to :recipes
+  belongs_to :ingredient
+  belongs_to :recipe
 
   validates :recipe, :ingredient, :amount, :measurement, presence: true
 end
