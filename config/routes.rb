97ab2c9639c2267ccee945a1f356a5b90a4f41b1
Marvan Taghvaie & Ali Baker <pair+marvant.thealicat13@devbootcamp.com>
2016-10-27 14:22:22 -0700

    Removed extra routes, fix nested recipe route on ingredient show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,4 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
-
-  get 'recipes/create'
-
-  get 'recipes/new'
-
-  get 'recipes/edit'
-
-  get 'recipes/show'
-
-  get 'recipes/update'
-
-  get 'recipes/destroy'
-
-  get 'ingredients/index'
-
-  get 'ingredients/show'
-
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
