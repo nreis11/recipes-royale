4f03e4df67554130087a606f864568d6d27f1e55
Ali Baker <alicia.baker13@gmail.com>
2016-10-27 10:25:47 -0700

    Revert "Courses controller views"

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  get 'courses/show'
-
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
 
-  resources :courses, only: [:index, :show] do
+  resources :courses, only: [:show] do
     resources :recipes
   end
 
-  resources :ingredients, only: [:index, :show] do
+  resources :ingredients, only: [:show] do
     resources :recipes
   end
 
