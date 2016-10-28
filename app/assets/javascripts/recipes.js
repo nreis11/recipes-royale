ae3d6b384f9ea803dcc574c84decbaa71bb3c92e
Nick Reis & Ali Baker <pair+reeses-pieces.thealicat13@devbootcamp.com>
2016-10-28 11:47:03 -0700

    deleted coffee script and added ajax for adding new recipe

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,5 +1,32 @@
-// $(function(){
+$(function() {
+  submitRecipeListener();
+});
 
-// };
+var submitRecipeListener = function() {
+  $(".well").on("submit", "#add-recipe-form", function(event) {
+    event.preventDefault();
+    console.log("Success!");
 
-// var submitRecipeListener = function()
+    var submitRecipeTarget = $(this);
+    var submitRecipeParent = $(this).parent();
+    var address = submitRecipeTarget.attr("action")
+    var method = submitRecipeTarget.attr("method")
+    var recipeData = submitRecipeTarget.serialize()
+
+    // console.log(submitRecipeTarget)
+    // console.log(address)
+    // console.log(method)
+    // console.log(data)
+
+    var request = $.ajax({
+      url: address,
+      method: method,
+      data: recipeData
+    })
+  })
+
+  request.done(function(response) {
+    console.log("success")
+
+  })
+}
